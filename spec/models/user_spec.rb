dd21d1847f052dccccb0057d918e16304500f70b
LDN <ledenevdn@gmail.com>
2020-07-19 12:51:15 +0700

    rspec validate user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+  end
 end
